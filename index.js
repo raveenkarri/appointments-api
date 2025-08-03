55f204409f2c9fa2bde97db6017cd5fa63020e7f
raveenkarri <raveenkarri@gmail.com>
2025-08-03 17:01:01 +0530

    Updated project and fixed deployment issues

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const doctorRoutes = require("./routes/doctorRoutes.js");
-const userRoutes = require("./routes/userRoutes.js");
+
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 8080;
 
 app.use("/", doctorRoutes);
-app.use("/",userRoutes)
+
 
 
 app.listen(port, () => { 
